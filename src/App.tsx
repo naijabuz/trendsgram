71737184a28171b1d1ed38ad47e3cec0e2db9d26
yusuf <faladeyusuf54@gmail.com>
2023-12-20 13:35:43 +0100

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./globals.css";
 
+import SignInForm from "./_auth/forms/SignInForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
+
+import { Home } from "./_root/pages";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route path="/sign-in" element={<SignInForm />} />
+        <Route path="/sign-up" element={<SignUpForm />} />
+
+        {/* private routes */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
